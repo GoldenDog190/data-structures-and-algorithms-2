1420b8552765f0315bfcf3d03e99dcb41173dd47
Amelia Waggoner <wildginger@wavecable.com>
2021-05-04 08:00:20 -0700

    added to the code challenge

--- challenges/lib/src/main/java/challenges/RepeatedWord/RepeatedWord.java
+++ challenges/lib/src/main/java/challenges/RepeatedWord/RepeatedWord.java
@@ -1,11 +1,12 @@
 package challenges.RepeatedWord;
 
 
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Locale;
-import java.util.Set;
+
+
+import javax.sound.sampled.Line;
+import java.util.*;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class RepeatedWord {
 
@@ -20,6 +21,18 @@ public class RepeatedWord {
         }
         return null;
 
+    }
+
+    public Map<String, Integer> countFrequency(String string) {
+        Map<String, Integer> count = new HashMap<>();
+        Pattern.compile("[^a-zA-Z]+").splitAsStream(string).forEach(s -> count.merge(s.toLowerCase(Locale.ROOT), 1, Integer::sum));
+        return count;
+    }
+
+    public List<String> commonWords(String string, int k){
+        Map<String, Integer> count = new HashMap<>();
+        Pattern.compile("[^a-zA-Z]+").splitAsStream(string).forEach(s -> count.merge(s.toLowerCase(Locale.ROOT), 1, Integer::sum));
+        return count.entrySet().stream().sorted(Comparator.comparing(Map.Entry::getValue)).limit(k).map(Map.Entry::getKey).collect(Collectors.toList(ArrayList::new));
     }
 
 
